f3c8431b61250ec18e444bffae875e5a516eb989
Sterling <sterling.nostedt@gmail.com>
2016-06-16 16:40:41 -0700

    added react markdown

--- src/js/containers/EditorViewContainer.jsx
+++ src/js/containers/EditorViewContainer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import MarkdownEditorContainer from './MarkdownEditorContainer.jsx';
 
 export default class EditorViewContainer extends React.Component {
-  render () {
+  render() {
     return(
       <div>
-      HYooooo 
+        <MarkdownEditorContainer/>
       </div>
     );
   }
